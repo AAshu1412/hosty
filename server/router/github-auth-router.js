19eb7ef47463a536962cfc4c43bcb429e5d5af83
AAshu1412 <ashutoshsanjeevmittal@gmail.com>
2026-03-15 16:22:18 +0530

    user repo and content in it

--- server/router/github-auth-router.js
+++ server/router/github-auth-router.js
@@ -3,5 +3,6 @@ const router=express.Router();
 const authcontrollers=require("../controller/github-auth-controller");
 
 router.route("/callback").post(authcontrollers.github_callback);
+router.route("/repoContent").get(authcontrollers.github_user_repos);
 
 module.exports=router;
