2d196a3db52663016a37273a1e4a204a9ac9a62c
AAshu1412 <ashutoshsanjeevmittal@gmail.com>
2026-03-16 18:17:05 +0530

    console output done

--- server/controller/jenkins-controller.js
+++ server/controller/jenkins-controller.js
@@ -1,42 +1,58 @@
 const axios = require('axios');
+
 const jenkins_get_crumb = async (req, res) => {
 
-try{
+    try {
 
-const getCrumb = await axios.get('http://localhost:8090/crumbIssuer/api/json', {
-    auth: {
-        username: 'ashu',
-        password: process.env.JENKINS_API_TOKEN
-      }
-});
+        const getCrumb = await axios.get('http://localhost:8090/crumbIssuer/api/json', {
+            auth: {
+                username: 'ashu',
+                password: process.env.JENKINS_API_TOKEN
+            }
+        });
 
-console.log("Crumb: "+JSON.stringify(getCrumb.data, null, 2));
+        console.log("Crumb: " + JSON.stringify(getCrumb.data, null, 2));
 
-const buildResponse = await axios.post(`http://localhost:8090/job/Hosty/buildWithParameters?token=${process.env.JENKINS_API_TOKEN}`,
-    { name: "GOOOOGI" }, // Body (parameters)
-    {
-      auth: {
-        username: 'ashu',
-        password: process.env.JENKINS_API_TOKEN
-      },
-      headers: {
-        [getCrumb.data.crumbHeader]: getCrumb.data.crumb,  // Header (CSRF protection)
-        'Content-Type': 'application/x-www-form-urlencoded'
-      }
+        const buildResponse = await axios.post(`http://localhost:8090/job/Hosty/buildWithParameters?token=${process.env.JENKINS_API_TOKEN}`,
+            { name: "GOOOOGI" }, // Body (parameters)
+            {
+                auth: {
+                    username: 'ashu',
+                    password: process.env.JENKINS_API_TOKEN
+                },
+                headers: {
+                    [getCrumb.data.crumbHeader]: getCrumb.data.crumb,  // Header (CSRF protection)
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                }
+            }
+        );
+        console.log("\n************************************************************\n");
+        console.log("Build Response: " + JSON.stringify(buildResponse.data, null, 2));
+        console.log("\n************************************************************\n");
+
+        res.status(200).json({ message: 'Build started' });
+
+    } catch (error) {
+        console.error('❌ Error:', error);
+        res.status(500).json({ error });
     }
-  );
-  console.log("\n************************************************************\n");
-  console.log("Build Response: "+JSON.stringify(buildResponse.data, null, 2));
-  console.log("\n************************************************************\n");
-
-res.status(200).json({ message: 'Build started' });
-
-}catch(error){
-    console.error('❌ Error:', error);
-    res.status(500).json({ error });
 }
 
-
+const jenkins_console_output = async (req, res) => {
+    try{
+        const consoleOutput = await axios.get(`http://localhost:8090/job/Hosty/lastBuild/consoleText`, {
+            auth: {
+                username: 'ashu',
+                password: process.env.JENKINS_API_TOKEN
+            }
+        });
+        console.log("Console Output: "+consoleOutput.data);
+        res.status(200).json({ message: 'Console output fetched' });
+    }
+    catch(error){
+        console.error('❌ Error:', error);
+        res.status(500).json({ error });
+    }
 }
 
-module.exports = { jenkins_get_crumb };
+module.exports = { jenkins_get_crumb,jenkins_console_output };
--- server/router/jenkins-router.js
+++ server/router/jenkins-router.js
@@ -3,5 +3,6 @@ const router=express.Router();
 const jenkinscontrollers=require("../controller/jenkins-controller");
 
 router.route("/getCrumb").get(jenkinscontrollers.jenkins_get_crumb);
+router.route("/consoleOutput").get(jenkinscontrollers.jenkins_console_output);
 
 module.exports=router;
