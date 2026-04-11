8cbb80315e0d86dc7aefff55a09ae531f4467e32
AAshu1412 <ashutoshsanjeevmittal@gmail.com>
2026-04-11 15:51:50 +0530

    docker install bash file

--- others/terraform/docker_installation.sh
+++ others/terraform/docker_installation.sh
@@ -3,9 +3,12 @@
 sudo apt-get update
 sudo apt-get upgrade -y
 
-sudo apt-get install docker.io -y
+sudo apt-get install docker.io docker-compose-plugin -y
+sudo usermod -aG docker ubuntu
 
-sudo usermod -aG docker $USER
 
 curl -fsSL https://deb.nodesource.com/setup_24.x | sudo -E bash -
 sudo apt install -y nodejs
+
+sudo systemctl enable docker
+sudo systemctl start docker
